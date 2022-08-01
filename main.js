9dfa3ce7a2b2c38db10777c8e1ad9f2c358027a3
Travis Wagner <travis@travisaw.com>
2022-08-01 01:26:29 -0400

    cleanup

--- main.js
+++ main.js
@@ -1,38 +1,29 @@
-import './style.css';
-import * as Tone from 'tone';
-
 const synth = new Tone.Synth().toDestination();
-
 const keys = Array.from(document.querySelectorAll('.key'));
 
 document.addEventListener('mousedown', (e) => {
-	if (!e.target.classList.contains('key')) {
-		return;
-	}
-	e.target.classList.add('active');
-	synth.triggerAttack(e.target.dataset.note);
+  if (!e.target.classList.contains('key')) {
+    return;
+  }
+  synth.triggerAttack(e.target.dataset.note);
+  e.target.classList.add('active');
 });
 
 document.addEventListener('mouseup', () => {
-	for (const key of keys) {
-		key.classList.remove('active');
-	}
-	synth.triggerRelease();
+  keys.forEach(key => key.classList.remove('active'));
+  synth.triggerRelease();
 });
 
 document.addEventListener('keydown', (e) => {
-	const key = keys.find(key => key.dataset.key === e.key);
-	console.log(key);
-	if (!key) {
-		return;
-	}
-	key.classList.add('active');
-	synth.triggerAttack(key.dataset.note);
+  const key = keys.find(key => key.dataset.key === e.key);
+  if (!key) {
+    return;
+  }
+  key.classList.add('active');
+  synth.triggerAttack(key.dataset.note);
 });
 
 document.addEventListener('keyup', () => {
-	for (const key of keys) {
-		key.classList.remove('active');
-	}
-	synth.triggerRelease();
+  keys.forEach(key => key.classList.remove('active'));
+  synth.triggerRelease();
 });
