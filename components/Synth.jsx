6db0d282bff9b25c3d7d7f4866dc4db72294758c
Travis Wagner <travis@travisaw.com>
2022-08-06 00:55:47 -0400

    add active state to els

--- components/Synth.jsx
+++ components/Synth.jsx
@@ -1,80 +1,87 @@
-import React from 'react';
+import React, {createContext, useState, useMemo} from 'react';
 import { generateNotes } from '../lib/utils';
 import {PolySynth, Synth} from 'tone';
+import { Key } from './Key';
 
-const SynthContext = React.createContext();
+const SynthContext = createContext();
 
 export const SynthController = () => {
-	const [options, setOptions] = React.useState({
+	const [options, setOptions] = useState({
 		oscillator: 'square',
 		detune: 0,
 		volume: -3,
 		octave: 3
 	});
 
-	const [notes, setNotes] = React.useState(generateNotes(18, options.octave));
+	const [notes, setNotes] = useState(generateNotes(18, options.octave));
 	
 	React.useEffect(() => {
 		setNotes(generateNotes(18, options.octave));
 	}, [options.octave]);
-	
+
 	React.useEffect(() => {
 		const synth = new PolySynth(Synth, {
 			oscillator: {
 				type: options.oscillator,
 			}
 		});
-		synth.toDestination();
 
+		synth.toDestination();
+		
 		const keyboardSynthTrigger = (e) => {
 			const type = e.type === 'keydown' ? 'attack' : 'release';
-			const { note } = notes.find(note => note.key === e.key) || {};
-		
-			if (!note || e.repeat) {
-				return false;
-			}
-		
+			const { note } = notes.find(note => note.char === e.key) || {};
+			if (!note || e.repeat) return;
 			if (type === 'attack') {
-				// el.classList.add('active');
+				document.querySelector(`[data-char="${e.key}"]`).classList.add('active');
 				synth.triggerRelease([note]);
+				synth.triggerAttack([note]);
+				return;
+			}
+			if (type === 'release') {
+				document.querySelector(`[data-char="${e.key}"]`).classList.remove('active');
+				synth.triggerRelease([note]);
+				return;
+			}
+			return false;
+		};
+
+		const mouseSynthTrigger = (e) => {
+			const type = e.type === 'mousedown' ? 'attack' : 'release';
+		
+			if (type === 'attack' && e.target.classList.contains('key')) {
+				const { note } = notes.find(note => note.char === e.target.innerHTML) || {};
+				synth.releaseAll();
 				synth.triggerAttack(note);
 				return true;
 			}
 		
 			if (type === 'release') {
-				synth.triggerRelease([note]);
+				synth.releaseAll();
 				return true;
 			}
 		
 			return false;
-		};
+		};		
 
-		window.addEventListener('keydown', keyboardSynthTrigger);
-		window.addEventListener('keyup', keyboardSynthTrigger);
+		document.addEventListener('keydown', keyboardSynthTrigger);
+		document.addEventListener('keyup', keyboardSynthTrigger);
+		document.addEventListener('mousedown', mouseSynthTrigger);
+		document.addEventListener('mouseup', mouseSynthTrigger);
+
 		return () => {
-			window.removeEventListener('keydown', keyboardSynthTrigger);
-			window.removeEventListener('keyup', keyboardSynthTrigger);
+			document.removeEventListener('keydown', keyboardSynthTrigger);
+			document.removeEventListener('keyup', keyboardSynthTrigger);
+			document.removeEventListener('mousedown', mouseSynthTrigger);
+			document.removeEventListener('mouseup', mouseSynthTrigger);
 		};
 	}, [notes, options]);
-
-	const elementRef = React.useRef();
-
-	React.useEffect(() => {
-		const divElement = elementRef.current;
-	}, []);
-
-	const KeyboardComponent = Keyboard;
 
 	return (
 		<SynthContext.Provider value={{options, setOptions, notes, setNotes}}>
 			<div className="synth">
 				<ControlPanel />
-				{[1].map((x, i) => {
-					x = <KeyboardComponent key={i} />;
-					console.log(x);
-					return x;
-				})}
-				<KeyboardComponent />
+				<Keyboard />
 			</div>
 		</SynthContext.Provider>
 	);
@@ -91,7 +98,7 @@ export const ControlPanel = () => {
 		<div className="controls">
 			<ControlButtons label="Octave">
 				<Button onClick={() => updateSynth('octave', options.octave - 1)}>-</Button>
-				<Button onClick={() => updateSynth(octave, options.octave + 1)}>+</Button>
+				<Button onClick={() => updateSynth('octave', options.octave + 1)}>+</Button>
 			</ControlButtons>
 		</div>
 	);
@@ -115,13 +122,12 @@ export const Keyboard = () => {
 
 	return (
 		<div className="keyboard">
-			{ notes.map(({note, key}, i) => {
-				return <Key note={note} keyname={key} key={i} />;
+			{ notes.map(({note, color, char, active}, i) => {
+				const props = { note, color, char, active };
+				return <Key {...props} key={i} />;
 			}) }
 		</div>
 	);
 };
 
-export const Key = ({note, keyname}) => {
-	return <div className={`key ${note.includes('#') ? 'black' : 'white'}`}>{keyname}</div>;
-};
+
