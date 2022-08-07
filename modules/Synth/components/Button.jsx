583a63c1153ad9f78c6990b24e2eeea090601d00
Travis Wagner <travis@travisaw.com>
2022-08-07 01:39:20 -0400

    add active state

--- modules/Synth/components/Button.jsx
+++ modules/Synth/components/Button.jsx
@@ -1,9 +1,15 @@
 import styles from './Button.module.css';
 
-export function Button({ children, type, ...props }) {
+export function Button({ active, type, children, ...props }) {
 	let style = styles.button;
+	
 	if (type && styles[type]) {
 		style = `${style} ${styles[type]}`;
 	}
+
+	if (active) {
+		style = `${style} ${styles.active}`;
+	}
+
 	return <div className={style} {...props}>{children}</div>;
 }
