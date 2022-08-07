f0b970d2be610157d9a2318aa1b79c1fd7e872aa
Travis Wagner <travis@travisaw.com>
2022-08-06 23:10:52 -0400

    move sound generator

--- modules/Synth/components/Keyboard.jsx
+++ modules/Synth/components/Keyboard.jsx
@@ -5,28 +5,7 @@ import { useEffect, useContext, useRef, useState } from 'react';
 import { SynthContext } from './SynthController';
 
 export function Keyboard() {
-	const { options, notes } = useContext(SynthContext);
-	const {keyPressed, keyReleased} = useKeyPress();
-	const synth = useSynth(options);
-
-	useEffect(() => {
-		console.log('notes', notes);
-		const note = notes.find(note => note.char === (keyPressed || keyReleased));
-
-		if (!note) return;
-
-		try {
-			if (keyPressed) {
-				synth.triggerAttack([note.note]);
-			}
-
-			if (keyReleased) {
-				synth.triggerRelease([note.note]);
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	}, [keyPressed, keyReleased, notes, synth]);
+	const { notes } = useContext(SynthContext);
 
 	return (
 		<div className={styles.flex}>
