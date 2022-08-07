91039e3deb487808abf4ae19d120c5e5efa648e1
Travis Wagner <travis@travisaw.com>
2022-08-07 01:39:28 -0400

    add multiselect

--- modules/Synth/components/Control.jsx
+++ modules/Synth/components/Control.jsx
@@ -1,4 +1,6 @@
 import styles from './SynthController.module.css';
+import { Button } from './Button';
+import {Radio } from './Radio';
 
 export function Group({ label, value, children }) {
 	return (
@@ -13,10 +15,24 @@ export function Group({ label, value, children }) {
 
 export function Label({ label, value }) {
 	console.log(styles);
-	return (label || value) && <h3 className="control-info"><span className="control-label">{label}</span>{(label && value) && ': '}<span className={styles['fw-normal']}>{value}</span></h3>;
+	return (label || value) && <h3 className={styles['control-info']}><span className="control-label">{label}</span>{(label && value) && ': '}<span className={styles['fw-normal']}>{value}</span></h3>;
+}
+
+export function MultiSelect({ active, name, label, value, options, onChange }) {
+	return (
+		<div className={styles['control-item']}>
+			<Label label={label} value={value} />
+			<div className={styles.flex}>
+				{options.map(({ label, value }) => {
+					return <Button active={active === value} name={name} key={value} onClick={() => onChange(value)}>{label}</Button>;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export const Control = {
 	Group,
 	Label,
+	MultiSelect,
 };
